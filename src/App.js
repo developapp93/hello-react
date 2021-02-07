d9be9e28b62ee0bdf42f7830a5100e800c324398
developapp <developapp93@gmail.com>
2021-02-07 07:12:55 +0700

    class component dalam 1 file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
 
+class Header extends Component {
+    render() {
+        return (
+            <h2>Class Component Dalam 1 File</h2>
+        );
+    }
+}
+
 class App extends Component {
     render() {
         return (
             <div>
+                <Header />
                 <h1>Rendering Elements</h1>
             </div>
         );
